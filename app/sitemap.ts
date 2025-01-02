a10e26ad36377c32494b11c46d82b7479178348f
Biben Nurbani Hasan <bibennurbani@gmail.com>
2025-01-02 14:31:14 +0700

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,18 +7,38 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}`,
       lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 1,
     },
     {
       url: `${baseUrl}/about`,
       lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
     {
-      url: `${baseUrl}/blog`,
+      url: `${baseUrl}/classes`,
       lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/games`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/teachers`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
     },
   ];
 }
